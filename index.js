632fe8e24c8638b3a2188cb69a1370105888ac8c
Seunghyun <seunghyunleekr@gmail.com>
2019-08-26 23:50:59 +0900

    test knn-classifier

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let net;
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 
 async function setupWebcam() {
 	return new Promise((resolve, reject) => {
@@ -33,13 +34,29 @@ async function app() {
 	// console.log(result);
 
 	await setupWebcam();
-	while (true) {
-		const result = await net.classify(webcamElement);
 
-		document.getElementById('console').innerText = `
-			prediction: ${result[0].className}\n
-			probability: ${result[0].probability}
-		`;
+	const addExample = classId => {
+		const activation = net.infer(webcamElement, 'conv_preds');
+		classifier.addExample(activation, classId);
+	};
+	// When clicking a button, add an example for that class.
+	document.getElementById('class-a').addEventListener('click', () => addExample(0));
+	document.getElementById('class-b').addEventListener('click', () => addExample(1));
+	document.getElementById('class-c').addEventListener('click', () => addExample(2));
+
+	while (true) {
+		if (classifier.getNumClasses() > 0) {
+			// Get the activation from mobilenet from the webcam.
+			const activation = net.infer(webcamElement, 'conv_preds');
+			// Get the most likely class and confidences from the classifier module.
+			const result = await classifier.predictClass(activation);
+
+			const classes = ['A', 'B', 'C'];
+			document.getElementById('console').innerText = `
+			prediction: ${classes[result.classIndex]}\n
+			probability: ${result.confidences[result.classIndex]}
+			`;
+		}
 
 		await tf.nextFrame();
 	}
